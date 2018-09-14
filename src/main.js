b0f2774cd66bb7d039c1fe070985f947e7699c39
Jesse Zhu <jesse@itjesse.com>
2018-09-14 14:32:52 +0900

    Change uuid

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import VueNativeSock from 'vue-native-websocket'
-Vue.use(VueNativeSock, 'wss://push.coinjinja.com/sub/direct_transfer?user_id=f1431814-716f-34a9-8b71-eb59c21f8d74', {
+Vue.use(VueNativeSock, 'wss://push.coinjinja.com/sub/direct_transfer?user_id=07065d64-fd33-39b5-b275-9a2cc4806ef4', {
   reconnection: true,
 })
 
